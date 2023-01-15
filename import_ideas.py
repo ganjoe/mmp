a976d3d7ce4beaedb45928ac2d6a786a10232b63
Daniel Meier <35268604+ganjoe@users.noreply.github.com>
2023-01-15 12:01:54 +0100

    adf

---
+++ import_ideas.py
@@ -0,0 +1,14 @@
+###############################
+#
+# https://www.hedgefundtelemetry.com/trade-ideas/
+# excel-tabelle erstellen: "ideas.xlxs" alle spalten als text formatieren
+# die tabelle per copy und paste einfügen. 
+# die erste zeile enthält mindestens die  header:
+# Symbol	Issue	Position    Stop 	Current Weight of Portfolio (%)
+#
+# Folgene Spalten werden 1:1 in transfer.xlxs übernommen
+# Symbol -> ticker  (z.b. TSLA)
+# Issue -> basiswert ( z.b. Alphabet Inc Class A )
+# Position -> side ( short oder long auf basiswert)
+# Current...-> soll ( prozentuale gewichtung)
+
